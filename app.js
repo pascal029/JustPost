3546ccddae78a6d327af332c8c56a548e5dc1d43
pascal029 <pascalvirgiawan05@gmail.com>
2022-09-14 19:43:16 +0700

    router controller, view register

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3001
 const router = require('./routes')
 
 app.set('view engine', 'ejs')
---
+++ controllers/controllerUser.js
@@ -0,0 +1,7 @@
+class Controller{
+    static register(req,res){
+        res.render('register')
+    }
+}
+
+module.exports = Controller
