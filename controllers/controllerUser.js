f55fa5c0e49af0b341a720d4a70decc3e6dd6f02
elsaelsa95 <elsa.lustiana95@gmail.com>
2022-09-15 19:27:18 +0700

    edit post

--- controllers/controllerUser.js
+++ controllers/controllerUser.js
@@ -8,46 +8,46 @@ class Controller {
             include: [Post]
         })
             .then(result => {
-                if (!result){
-                    res.redirect (`/user/${+req.params.profileId}/createProfile`)
+                if (!result) {
+                    res.redirect(`/user/${+req.params.profileId}/createProfile`)
                 }
-                else{
-                res.render('profile', { result })
+                else {
+                    res.render('profile', { result })
                 }
             })
             .catch(err => {
                 res.send(err)
             })
     }
-    static createProfile(req, res){
+    static createProfile(req, res) {
         const errors = req.query
         let profileId = +req.params.profileId
         // res.send(errors)
-        res.render ('createProfile', {profileId, errors})
+        res.render('createProfile', { profileId, errors })
     }
-    static saveNewProfile(req, res){
+    static saveNewProfile(req, res) {
         Profile.create({
             name: req.body.name,
             dob: req.body.dob,
             phone: req.body.phone,
             city: req.body.city,
             photo: req.body.photo,
-            UserId : +req.params.profileId
+            UserId: +req.params.profileId
         })
             .then(result => {
                 res.redirect(`/user/${+req.params.profileId}/profile`)
             })
             .catch(err => {
-                if(err.name == 'SequelizeValidationError'){
+                if (err.name == 'SequelizeValidationError') {
                     let errors = []
-                    err.errors.map(el =>{
+                    err.errors.map(el => {
                         errors.push(el.message)
                     })
                     res.redirect(`/user/${+req.params.profileId}/createProfile?errors=${errors}`)
                 } else {
                     res.send(err)
                 }
-                
+
             })
     }
     static editProfile(req, res) {
@@ -81,23 +81,23 @@ class Controller {
     }
     static createPost(req, res) {
         const id = req.params.profileId
-        const {errors} = req.query
-        res.render('add-post', {id, errors})
+        const { errors } = req.query
+        res.render('add-post', { id, errors })
     }
     static savePost(req, res) {
-        const {post, imageUrl} = req.body
+        const { post, imageUrl } = req.body
         const ProfileId = req.params.profileId
-        console.log(post, imageUrl, req.body)
+        // console.log(post, imageUrl, req.body)
         Post.create({
             post, imageUrl, ProfileId
         })
-        .then(result => {
-            res.redirect(`/dashboard/${+req.params.profileId}`)
-        })
+            .then(result => {
+                res.redirect(`/dashboard/${+req.params.profileId}`)
+            })
             .catch(err => {
-                if(err.name == `SequelizeValidationError`){
+                if (err.name == `SequelizeValidationError`) {
                     let errors = []
-                    err.errors.map(el =>{
+                    err.errors.map(el => {
                         errors.push(el.message)
                     })
                     res.redirect(`/user/${ProfileId}/profile/addPost?errors=${errors}`)
@@ -107,10 +107,29 @@ class Controller {
             })
     }
     static editPost(req, res) {
-        res.send('masuk')
+        let profileId = +req.params.profileId
+        Post.findOne({ where: { id: +req.params.postId } })
+            .then(result => {
+                res.render('editPost', { result, profileId })
+            })
+            .catch(err => {
+                res.send(err)
+            })
     }
     static updatePost(req, res) {
-        res.send('masuk')
+        Post.update({
+            post: req.body.post,
+            imageUrl: req.body.imageUrl,
+        },
+            {
+                where: { id: +req.params.postId }
+            })
+            .then(result => {
+                res.redirect(`/user/${+req.params.profileId}/profile`)
+            })
+            .catch(err => {
+                res.send(err)
+            })
     }
     static deletePost(req, res) {
         Post.destroy({
